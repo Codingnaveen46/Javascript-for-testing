4f08314329d0df4ec89d8c77d3297b0b6739e61b
Codingnaveen46 <naveenreddy44112gmail.com>
2025-07-22 22:44:52 +0530

    learnt about the keywords in javascript

---
+++ Keywords.js
@@ -0,0 +1,17 @@
+// keywords in javascript
+
+
+// break	case	catch	class	const	continue	debugger
+// default	delete	do	else	enum	export	extends
+// false	finally	for	function	if	import	in
+// instanceof	let	new	null	package	private	protected
+// public	return	static	super	switch	this	throw
+// true	try	typeof	var	void	while	with
+// yield	await	get	set	arguments	implements	interface
+// let	package	private	protected	public	return	static
+// synchronized	throws	transient	volatile	abstract	boolean	byte
+
+
+
+
+// learnt about the keywords in javsscript 
