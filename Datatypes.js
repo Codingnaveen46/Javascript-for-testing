68fb2b51273918e0f2685c6d7f28f6452ebc3602
Codingnaveen46 <naveenreddy44112gmail.com>
2025-07-22 20:40:46 +0530

    addded first learning data types

---
+++ Datatypes.js
@@ -0,0 +1,14 @@
+// PRIMITIVE DATA TYPES 
+const string = "Hello, World!"; // String 
+const number = 42; // Number
+const boolean = true; // Boolean
+// SYMBOL DATA TYPE
+const symbol = Symbol("uniqueIdentifier"); // Symbol
+// BIGINT DATA TYPE
+const bigInt = BigInt(9007199254740991); // BigInt
+// OBJECT DATA TYPE
+const object = {
+    name: "John Doe",
+    age: 30,
+    isEmployed: true
+}; // Object
